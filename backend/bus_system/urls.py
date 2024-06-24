102ead2f0d94ba22a7512ea1d931e10a423a2bef
Brendon <Bengelbrecht2002@gmail.com>
2024-06-24 15:48:54 +0200

    Confirmation Email setup

--- backend/bus_system/urls.py
+++ backend/bus_system/urls.py
@@ -10,6 +10,7 @@ urlpatterns = router.urls
 urlpatterns = [
     path('', include(router.urls)),
     path('apply-bus/', ApplicationView.as_view(), name='apply-bus'),
-    # path('send-email/', SendEmailView.as_view(), name='send-email'),
+    path('send-confirmation-email/', send_confirmation_email_view,
+         name='send_confrimation_email'),
     path('learner-list/', LearnerListView.as_view(), name='learner-list'),
 ]
